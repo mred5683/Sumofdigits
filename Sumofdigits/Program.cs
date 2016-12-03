4ec90976a49ea6573115b09eebf5469d372d20d7
mred5683 <hansenrd5683@gmail.com>
2016-12-03 01:24:00 -0800

    add sumofdigits to repo

---
+++ Sumofdigits/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumofdigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int64 num, sum = 0, r;
+
+            Console.WriteLine("Enter a Number : ");
+
+            num = Int64.Parse(Console.ReadLine());
+
+            while (num != 0)
+
+            {
+                r = num % 10;
+                num = num / 10;
+                sum = sum + r;
+            }
+
+            Console.WriteLine("Sum of Digits of the Number : " + sum);
+            Console.ReadLine();
+        }
+    }
+}
